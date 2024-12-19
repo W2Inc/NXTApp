d29681d8f1fe9ba00b98a5ff8fc3f6e732240db0
W2Wizard <main@w2wizard.dev>
2024-12-20 00:18:13 +0100

    Add Katex

--- frontend/src/lib/components/markdown/context.svelte.ts
+++ frontend/src/lib/components/markdown/context.svelte.ts
@@ -2,6 +2,8 @@ import { Carta } from "carta-md";
 import { emoji } from "@cartamd/plugin-emoji";
 import { code } from "@cartamd/plugin-code";
 import DOMPurify from "isomorphic-dompurify";
+import 'katex/dist/katex.css';
+import { math } from "@cartamd/plugin-math";
 
 /** Singleton carta context for markdown readers/writers. */
 export const cartaContext = $state(
@@ -16,6 +18,7 @@ export const cartaContext = $state(
 			emoji(),
 			// slash(),
 			code(),
+			math(),
 		],
 	}),
 );
